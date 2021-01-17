9fe2c991d5bf51b25cf98e1f82c592a2ad0afe4e
maxxxilina <77548944+maxxxilina@users.noreply.github.com>
2021-01-17 09:59:17 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,14 +2,13 @@ OLED.init(128, 64)
 tinkercademy.crashSensorSetup(DigitalPin.P0)
 basic.forever(function () {
     if (tinkercademy.ADKeyboard(ADKeys.A, AnalogPin.P2)) {
-        OLED.writeStringNewLine("--- inga problem --- ")
-        pins.digitalWritePin(DigitalPin.P1, 1)
-        control.waitMicros(500000)
-        pins.digitalWritePin(DigitalPin.P1, 0)
-        control.waitMicros(500000)
-        pins.digitalWritePin(DigitalPin.P8, 0)
         if (tinkercademy.crashSensor()) {
-        	
+            OLED.writeStringNewLine("--- inga problem --- ")
+            pins.digitalWritePin(DigitalPin.P1, 1)
+            control.waitMicros(500000)
+            pins.digitalWritePin(DigitalPin.P1, 0)
+            control.waitMicros(500000)
+            pins.digitalWritePin(DigitalPin.P8, 0)
         } else {
             OLED.writeStringNewLine("varning varning alarm")
             pins.digitalWritePin(DigitalPin.P1, 0)
