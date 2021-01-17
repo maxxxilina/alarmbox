bec647c1d0c7008fad2f3d5641debdc46c0aaf1f
maxxxilina <77548944+maxxxilina@users.noreply.github.com>
2021-01-17 09:51:44 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,24 @@
 OLED.init(128, 64)
 tinkercademy.crashSensorSetup(DigitalPin.P0)
 basic.forever(function () {
-    if (tinkercademy.crashSensor()) {
-        OLED.writeStringNewLine("allt är lugnt")
+    if (tinkercademy.ADKeyboard(ADKeys.A, AnalogPin.P2)) {
+        OLED.writeStringNewLine("--- inga problem --- ")
         pins.digitalWritePin(DigitalPin.P1, 1)
         control.waitMicros(500000)
         pins.digitalWritePin(DigitalPin.P1, 0)
         control.waitMicros(500000)
         pins.digitalWritePin(DigitalPin.P8, 0)
+        if (tinkercademy.crashSensor()) {
+        	
+        } else {
+            OLED.writeStringNewLine("varning varning alarm")
+            pins.digitalWritePin(DigitalPin.P1, 0)
+            pins.digitalWritePin(DigitalPin.P8, 1)
+            control.waitMicros(50000)
+            pins.digitalWritePin(DigitalPin.P8, 0)
+            control.waitMicros(50000)
+        }
     } else {
-        OLED.writeStringNewLine("varning varning alarm")
-        pins.digitalWritePin(DigitalPin.P1, 0)
-        pins.digitalWritePin(DigitalPin.P8, 1)
-        control.waitMicros(50000)
-        pins.digitalWritePin(DigitalPin.P8, 0)
-        control.waitMicros(50000)
+        OLED.writeStringNewLine("skriv in kod")
     }
 })
